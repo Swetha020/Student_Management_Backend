c0bf2d3851c958b5c9487e9cf72c835a626cd51c
Swetha <swetha00200@gmail.com>
2026-06-30 12:53:53 +0530

    chore: dotenv fixed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("dotenv").config();
 
 const { initializeDatabase } = require("./db/db.connect");
 const { Student } = require("./models/students.model");
